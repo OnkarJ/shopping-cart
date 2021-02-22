219fff0528a8ea2619ce790b54d28dfc69b07754
Onkar Javalgi <onkar.javalgi@tieto.com>
2021-02-22 20:56:08 +0530

    lazily loading the main component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
+const App = lazy(() => import('./App'));
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<Suspense fallback={<div>Loading...</div>}><App /></Suspense>,document.getElementById('root'));
